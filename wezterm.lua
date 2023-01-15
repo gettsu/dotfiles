172e0448be0281ddb9d76f3e7b5b3c18fea89a07
gettsu <tsugedaisuke.dev@gmail.com>
2023-01-15 09:24:57 +0900

    update: wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -2,8 +2,11 @@ local wezterm = require 'wezterm';
 
 return {
     use_ime = true,
-    font_size = 15.0,
+    font_size = 11.0,
     color_scheme = "OneHalfDark",
     hide_tab_bar_if_only_one_tab = true,
+    warn_about_missing_glyphs = false,
+    initial_cols = 120,
+    initial_rows = 50,
     adjust_window_size_when_changing_font_size = false,
 }
