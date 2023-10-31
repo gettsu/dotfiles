61afc202789764bdcef1a734e5989c1d980ff5f5
gettsu <tsugedaisuke.dev@gmail.com>
2023-11-01 04:32:37 +0900

    update: configs

--- init.lua
+++ init.lua
@@ -70,8 +70,53 @@ require("lazy").setup({
 },
 {
     "rebelot/kanagawa.nvim",
-}
+},
+{
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+},
 })
+
+keymap("n", "<leader>f", ":lua require('fzf-lua').files()<CR>", {  silent = true })
+keymap("n", "<leader>g", ":lua require('fzf-lua').git_status()<CR>", {  silent = true })
+keymap("n", "<leader>r", ":lua require('fzf-lua').grep_project()<CR>", {  silent = true })
+
+local coc_opts = {
+	silent = true,
+	noremap = true,
+	expr = true,
+}
+
+-- GoTo code navigation
+keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
+keymap("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+keymap("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+keymap("n", "gr", "<Plug>(coc-references)", {silent = true})
+
+keymap("n", "<leader>b", "<C-o>", {  silent = true })
+
+vim.keymap.set("i", "<Tab>",
+function()
+    is_visible = vim.fn["coc#pum#visible"]()
+    if is_visible == 1 then
+        return vim.fn["coc#pum#confirm"]()
+    else
+        return "<Tab>"
+    end
+end, coc_opts)
+
+function _G.show_docs()
+    local cw = vim.fn.expand('<cword>')
+    if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+        vim.api.nvim_command('h ' .. cw)
+    elseif vim.api.nvim_eval('coc#rpc#ready()') then
+        vim.fn.CocActionAsync('doHover')
+    else
+        vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+    end
+end
+keymap("n", "K", ':lua _G.show_docs()<CR>', {silent = true})
+
 
 vim.cmd "colorscheme kanagawa"
 
