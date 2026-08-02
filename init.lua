fb3d554a85e6b5aa09eb4c30d30310d6bea5f3a1
gettsu <tsugedaisuke.dev@gmail.com>
2026-08-02 17:22:00 +0900

    refactor init.lua

--- init.lua
+++ init.lua
@@ -7,268 +7,394 @@ local options = {
   clipboard = "unnamedplus",
   autoindent = true,
   termguicolors = true,
+  signcolumn = "yes",
+  completeopt = { "menu", "menuone", "noselect" },
 }
 
 vim.opt.shortmess:append("c")
 vim.opt.shada = ""
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
-for k, v in pairs(options) do
-  vim.opt[k] = v
+
+for key, value in pairs(options) do
+  vim.opt[key] = value
 end
 
-local opts = { noremap = true, silent = true }
+-- Use Vim's regular-expression syntax highlighting.
+vim.cmd("syntax enable")
 
-if not vim.g.vscode then
-  local function paste()
-    return {
-      vim.fn.split(vim.fn.getreg(""), "\n"),
-      vim.fn.getregtype(""),
-    }
-  end
+if vim.g.vscode then
+  return
+end
 
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = paste,
-      ["*"] = paste,
-    },
+if vim.fn.has("nvim-0.11.3") == 0 then
+  error("This init.lua requires Neovim 0.11.3 or newer")
+end
+
+local function paste_from_unnamed_register()
+  return {
+    vim.fn.getreg('"', 1, true),
+    vim.fn.getregtype('"'),
   }
+end
 
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-    })
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste_from_unnamed_register,
+    ["*"] = paste_from_unnamed_register,
+  },
+}
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local uv = vim.uv or vim.loop
+
+if not uv.fs_stat(lazypath) then
+  local output = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { output, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
-  vim.opt.rtp:prepend(lazypath)
+end
 
-  --local keymap = vim.keymap
-  local keymap = vim.api.nvim_set_keymap
+vim.opt.rtp:prepend(lazypath)
 
-  --Remap space as leader key
-  keymap("", "<Space>", "<Nop>", opts)
-  vim.g.mapleader = " "
-  vim.g.maplocalleader = " "
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-  -- Modes
-  keymap("n", "<Space>h", "^", opts)
-  keymap("n", "<Space>l", "$", opts)
-  -- 窓の切り替え --
-  keymap("n", "<CR><CR>", "<C-w><C-w>", opts)
+local map = vim.keymap.set
+local silent = { silent = true }
 
-  require("lazy").setup({
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      opts = {},
+map({ "n", "x", "s", "o" }, "<Space>", "<Nop>", silent)
+map("n", "<leader>h", "^", { silent = true, desc = "First non-blank character" })
+map("n", "<leader>e", "$", { silent = true, desc = "End of line" })
+map("n", "<CR><CR>", "<C-w><C-w>", { silent = true, desc = "Next window" })
+map("n", "<leader>b", "<C-o>", { silent = true, desc = "Jump backward" })
+
+require("lazy").setup({
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = {
+        highlight = { "CursorColumn", "Whitespace" },
+        char = "",
+      },
+      whitespace = {
+        highlight = { "CursorColumn", "Whitespace" },
+        remove_blankline_trail = false,
+      },
+      scope = { enabled = false },
     },
-    {
-      "rebelot/kanagawa.nvim",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("fzf-lua").files()
+        end,
+        desc = "Find files",
+      },
+      {
+        "<leader>g",
+        function()
+          require("fzf-lua").git_status()
+        end,
+        desc = "Git status",
+      },
+      {
+        "<leader>r",
+        function()
+          require("fzf-lua").grep_project()
+        end,
+        desc = "Grep project",
+      },
+      {
+        "<leader>l",
+        function()
+          require("fzf-lua").buffers()
+        end,
+        desc = "List buffers",
+      },
     },
-    {
-      "ibhagwan/fzf-lua",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-      "williamboman/mason.nvim",
-      version = "^1.0.0",
-    },
-    {
-      "williamboman/mason-lspconfig.nvim",
-      version = "^1.0.0",
-    },
-    {
-      "tomasiser/vim-code-dark",
-    },
-    {
-      "nvimtools/none-ls.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-    },
-    {
-      "neovim/nvim-lspconfig",
-    },
-    {
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+  {
+    "tomasiser/vim-code-dark",
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local none_ls = require("null-ls")
+      local sources = {}
+
+      if vim.fn.executable("clang-format") == 1 then
+        table.insert(
+          sources,
+          none_ls.builtins.formatting.clang_format.with({
+            extra_args = {
+              "--style",
+              "{IndentWidth: 4, UseTab: Never, AllowShortFunctionsOnASingleLine: None}",
+            },
+          })
+        )
+      end
+
+      if vim.fn.executable("stylua") == 1 then
+        table.insert(
+          sources,
+          none_ls.builtins.formatting.stylua.with({
+            extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+          })
+        )
+      end
+
+      local format_group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+
+      none_ls.setup({
+        sources = sources,
+        debug = false,
+        on_attach = function(client, bufnr)
+          if not client:supports_method("textDocument/formatting") then
+            return
+          end
+
+          vim.api.nvim_clear_autocmds({ group = format_group, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = format_group,
+            buffer = bufnr,
+            desc = "Format with none-ls before saving",
+            callback = function()
+              vim.lsp.buf.format({
+                async = false,
+                bufnr = bufnr,
+                timeout_ms = 3000,
+                filter = function(format_client)
+                  return format_client.id == client.id
+                end,
+              })
+            end,
+          })
+        end,
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
     },
-    {
-      "petertriho/nvim-scrollbar",
-    },
-    { "hrsh7th/nvim-cmp" }, -- Required
-    { "hrsh7th/cmp-nvim-lsp" }, -- Required
-    {
-      "vim-airline/vim-airline",
-    },
-    {
-      "github/copilot.vim",
-    },
-    {
-      "f-person/git-blame.nvim",
-    },
-    {
-      "airblade/vim-gitgutter",
-    },
-    {
-      "romgrk/barbar.nvim",
-      dependencies = {
-        "lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
-        "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
-      },
-      init = function()
-        vim.g.barbar_auto_setup = false
-      end,
-    },
-    {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-    },
-    {
-      "esmuellert/codediff.nvim",
-      cmd = "CodeDiff",
-    }
-  })
+    config = function()
+      local cmp = require("cmp")
 
-  keymap("n", "<leader>f", ":lua require('fzf-lua').files()<CR>", { silent = true })
-  keymap("n", "<leader>g", ":lua require('fzf-lua').git_status()<CR>", { silent = true })
-  keymap("n", "<leader>r", ":lua require('fzf-lua').grep_project()<CR>", { silent = true })
-  keymap("n", "<leader>l", ":lua require('fzf-lua').buffers()<CR>", { silent = true })
-
-  keymap("n", "<leader>b", "<C-o>", { silent = true })
-
-  local scrollbar = require("scrollbar")
-  scrollbar.setup()
-
-  local cmp = require("cmp")
-  cmp.setup({
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "path" },
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }),
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-l>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+      })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
     },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ["<C-p>"] = cmp.mapping.select_prev_item(),
-      ["<C-n>"] = cmp.mapping.select_next_item(),
-      ["<C-l>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    }),
-  })
-  local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    config = function()
+      require("mason-lspconfig").setup({
+        -- Keep the original behavior: only explicitly enabled servers start.
+        automatic_enable = false,
+      })
 
-  if vim.fn.executable("clangd") == 1 then
-    vim.lsp.config.clangd = {
-      capabilities = capabilities,
-      cmd = { "clangd", "--background-index", "--clang-tidy", "--fallback-style=WebKit"},
-      filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" }, 
-    }
-    vim.lsp.enable("clangd")
-  end
-  if vim.fn.executable("rust-analyzer") == 1 then
-    vim.lsp.config["rust-analyzer"] = {
-      cmd = { "rust-analyzer" },
-      filetypes = { "rust" },
-      root_markers = { "Cargo.toml", "rust-project.json", ".git" },
-    }
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+      })
 
-    vim.lsp.enable("rust-analyzer")
-  end
-  if vim.fn.executable("ruff") == 1 then
-    vim.lsp.enable("ruff")
-  end
-  if vim.fn.executable("pyright") == 1 then
-    vim.lsp.config.pyright = {
-      capabilities = vim.lsp.protocol.make_client_capabilities(),
-      settings = {
-        python = {
-          analysis = {
-            typeCheckingMode = "off",
-            autoSearchPaths = true,
-            useLibraryCodeForTypes = true,
-            diagnosticMode = "off",
-            autoImportCompletions = false,
-          },
-          linting = {
-            enabled = false,
+      local clangd_cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+      }
+
+      vim.lsp.config("clangd", {
+        cmd = clangd_cmd,
+      })
+
+      vim.lsp.config("ruff", {
+        -- Let Pyright handle hover information when both Python servers attach.
+        on_attach = function(client)
+          client.server_capabilities.hoverProvider = false
+        end,
+      })
+
+      vim.lsp.config("pyright", {
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "off",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "openFilesOnly",
+              autoImportCompletions = false,
+            },
           },
         },
-      },
-      -- Disable all diagnostics from Pyright
-      handlers = {
-        ["textDocument/publishDiagnostics"] = function() end,
-      },
-    }
-    vim.lsp.enable("pyright")
-  end
-  vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
-  vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-  vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-  vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-  vim.keymap.set("n", "gK", "<cmd>lua vim.diagnostic.open_float()<CR>")
-  vim.cmd("colorscheme tokyonight")
+        -- Ruff remains the diagnostic provider for Python.
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function() end,
+        },
+      })
 
-  local highlight = {
-    "CursorColumn",
-    "Whitespace",
-  }
-  require("ibl").setup({
-    indent = { highlight = highlight, char = "" },
-    whitespace = {
-      highlight = highlight,
-      remove_blankline_trail = false,
-    },
-    scope = { enabled = false },
-  })
-  require("mason").setup()
-  require("mason-lspconfig").setup()
+      local servers = {
+        clangd = "clangd",
+        rust_analyzer = "rust-analyzer",
+        ruff = "ruff",
+        pyright = "pyright-langserver",
+      }
 
-  vim.lsp.config("*", {
-    capabilities = capabilities,
-  })
-
-  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-  local none_ls = require("null-ls")
-  none_ls.setup({
-    sources = {},
-    debug = false,
-  })
-
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client and client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = ev.buf })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = ev.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = ev.buf })
-                end,
-            })
+      for server, executable in pairs(servers) do
+        if vim.fn.executable(executable) == 1 then
+          vim.lsp.enable(server)
         end
+      end
     end,
+  },
+  {
+    "petertriho/nvim-scrollbar",
+    main = "scrollbar",
+    opts = {},
+  },
+  {
+    "vim-airline/vim-airline",
+  },
+  {
+    "github/copilot.vim",
+  },
+  {
+    "f-person/git-blame.nvim",
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
+  },
+  {
+    "romgrk/barbar.nvim",
+    dependencies = {
+      "lewis6991/gitsigns.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {},
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+  {
+    "esmuellert/codediff.nvim",
+    cmd = "CodeDiff",
+  },
+})
+
+local function format_buffer(bufnr)
+  local clients = vim.lsp.get_clients({
+    bufnr = bufnr,
+    method = "textDocument/formatting",
   })
-  require("barbar").setup({})
-  require('gitblame').setup {
-    message_template = '<summary> • <date> • <author>  • <sha>'
-  }
+  local preferred_client_id
+
+  -- clangd and none-ls can both format C/C++. Prefer the explicitly configured
+  -- none-ls formatter to avoid applying two formatting edits in succession.
+  for _, client in ipairs(clients) do
+    if client.name == "null-ls" then
+      preferred_client_id = client.id
+      break
+    end
+  end
+
+  vim.lsp.buf.format({
+    async = true,
+    bufnr = bufnr,
+    filter = preferred_client_id and function(client)
+      return client.id == preferred_client_id
+    end or nil,
+  })
 end
+
+local lsp_keymap_group = vim.api.nvim_create_augroup("LspKeymaps", { clear = true })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = lsp_keymap_group,
+  desc = "Set buffer-local LSP keymaps",
+  callback = function(event)
+    local function lsp_map(lhs, rhs, desc)
+      vim.keymap.set("n", lhs, rhs, {
+        buffer = event.buf,
+        silent = true,
+        desc = desc,
+      })
+    end
+
+    lsp_map("K", vim.lsp.buf.hover, "LSP hover")
+    lsp_map("gf", function()
+      format_buffer(event.buf)
+    end, "Format buffer")
+    lsp_map("gr", vim.lsp.buf.references, "LSP references")
+    lsp_map("gd", vim.lsp.buf.definition, "LSP definition")
+    lsp_map("gD", vim.lsp.buf.declaration, "LSP declaration")
+    lsp_map("gi", vim.lsp.buf.implementation, "LSP implementation")
+    lsp_map("gK", vim.diagnostic.open_float, "Line diagnostics")
+  end,
+})
